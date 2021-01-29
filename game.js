151692a006c2630bc0f42281ea0cfadb573b5c86
Ilia-707 <78213066+Ilia-707@users.noreply.github.com>
2021-01-29 19:31:59 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,28 +1,44 @@
-let rools = document.querySelector('.rools');
-rools.onclick = function(){
-	let elem = document.querySelector('#rools-text');
-	let coords = rools.getBoundingClientRect();
-	elem.style.top = coords.bottom + "px";
-	elem.style.left = coords.left + "px";
-	elem.style.visibility = 'visible'
-};
+document.body.onclick = function(event) {
+	let roolText = document.querySelector('#rool div');
+	if (roolText.style.display == 'block') {
+	roolText.style.display = 'none';
+	} else{
+	  if (event.target == document.getElementById('rool')){	
+	  roolText.style.display = 'block';
+	  }
+	}
+	
+}
 document.getElementById('start').onclick = start;
 
+//функция начала игры
 function start () {
+	//очищаем содержимое контейнера и скрываем таблицу результатов предыдущей игры
+	let container = document.getElementById('container');
+	container.innerHTML = '';
+	let listResult = document.getElementById('listResult');
+	listResult.style.visibility = 'hidden';
+	
+	//обьявляем массивы:
+	//карт игроков после раздачи
 	let arrNord = [];
 	let arrOst = [];
 	let arrYou = [];
 	let arrWest = [];
+	//карт игроков забраных со стола
 	let arrNordBet = [];
 	let arrOstBet = [];
 	let arrYouBet = [];
 	let arrWestBet = [];
+	//карт на столе после хода
 	let arrBet = [];
+	//карт на руках игроков в течение игры
 	let arrNordClone = [];
 	let arrOstClone = [];
 	let arrYouClone = [];
 	let arrWestClone = [];
-
+	
+    //создаем массив колоды карт и перемешиваем функцией shuffle
 	let arr = [];
 	let val = document.getElementById('val')
 	let N = val.value;
@@ -39,33 +55,33 @@ function start () {
 	};
 	let startList = shuffle(arr);	
 
-//??????? ????????? ?????, ??? ????????????? ?? ???????? ????? ? ????
-	let container = document.getElementById('container');
+//создаем скрытую карту, ее размеры пригодятся в дальнейшем	
 	let cardHidden = document.createElement('div');
 	cardHidden.classList.add('card');
 	cardHidden.style.visibility = 'hidden';
 	container.append(cardHidden);
 
-//??????? ????? ???? ??????? ????	
+//создаем колоду карт (элементы)	
 	for (let i = 1; i <= startList.length; i++) {
 		let card = document.createElement('div');
 		card.setAttribute('id', `card${i}`);
 		card.classList.add('card');
 		card.style.zIndex = startList[i-1];
-		card.style.top = container.offsetHeight/2 - cardHidden.offsetHeight/2 - (startList[i-1])/3 + "px";
-		card.style.left = container.offsetWidth/2 - cardHidden.offsetWidth/2 + (startList[i-1])/3 + "px";
+		card.style.top = container.offsetHeight/2 - cardHidden.offsetHeight/2 - (startList[i-1])/2 + "px";
+		card.style.left = container.offsetWidth/2 - cardHidden.offsetWidth/2 + (startList[i-1])/2 + "px";
 		container.append(card);
-	//??????? ??????? ???????
+	//создаем лицевую сторону
 		let face = document.createElement('div');
 		face.innerHTML = `<h3>${i}</h3><p>${i}</p>`;
 		face.classList.add('face');
 		card.append(face);
-	//??????? ???????? ???????
+	//создаем обратную сторону
 		let backface = document.createElement('div');
 		backface.classList.add('backface');
 		card.append(backface);
 	};
-
+	
+    //раздаем колоду
 	let promise = new Promise(function(resolve, reject) {
 	for (let i = startList.length; i > 0; i--) {		
 		setTimeout(function() {
@@ -94,10 +110,11 @@ function start () {
 	    card.style.left = container.offsetWidth*0.82 + "px";
 	    };
 		if (i == 1) resolve();
-        }, (startList.length-i)*100);		
+        }, (startList.length-i)*80);		
 	};	
 	});
 	
+	//сортируем свои карты по возрастанию и назначаем прослушиватель события "клик" своего хода
 	promise.then(function() {
 	arrYou.sort((a, b) => a - b);
 	for (let i = 0; i < arrYou.length; i++) {		
@@ -115,7 +132,8 @@ function start () {
 
 	container.addEventListener('click', yourStep);
 	});
-
+	
+//функция своего хода и передачи другому
 function yourStep (event) {
 	let target = event.target;
 	let elem = target.closest('.card');
@@ -139,7 +157,7 @@ function yourStep (event) {
 function removeArrayItem(arr, itemToRemove) {
 	return arr.filter(item => item !== itemToRemove);
 }
-
+//функция выбора игроками карты и передачи хода
 function chose (curArr) {
     	let curNum;
     	let choseArr = curArr.slice();
@@ -194,6 +212,7 @@ function chose (curArr) {
          };		 
 };
 
+//функция определения "чья взятка" и передачи хода
 function bet () {
 	arrBet.sort((a, b) => a - b);
 	let winNum = arrBet[3];
@@ -217,7 +236,7 @@ function bet () {
 			let card = document.getElementById(`card${item}`);
 			card.style.transform = "";
 			card.style.top = container.offsetHeight*0.02 + "px";
-			card.style.left = container.offsetWidth*0.62 + "px";
+			card.style.left = container.offsetWidth*0.70 + "px";
 		};
 		arrBet.length = 0;
 		if (arrYouClone.length == 0) {
@@ -245,7 +264,7 @@ function bet () {
 			let card = document.getElementById(`card${item}`);
 			card.style.transform = "";
 			card.style.top = container.offsetHeight*0.76 + "px";
-			card.style.left = container.offsetWidth*0.62 + "px";
+			card.style.left = container.offsetWidth*0.70 + "px";
 		};
 		arrBet.length = 0;
 		if (arrYouClone.length == 0) {
@@ -256,7 +275,7 @@ function bet () {
 	};
 };
 
-		
+//функция определения победителя		
 function winner() {
 	playResult = [
 	          {
@@ -284,8 +303,7 @@ function winner() {
 		tableResult.rows[i].cells[1].innerHTML = elem.player;
 		tableResult.rows[i].cells[2].innerHTML = elem.score;
 		i++;
-	};
-	let listResult = document.getElementById('listResult');
+	};	
 	document.getElementById('win').innerHTML = `Победил:  ${playResult[0].player}`;
 	listResult.style.top = document.body.offsetHeight/2 - listResult.offsetHeight/2 + "px";
 	listResult.style.left = document.body.offsetWidth/2 - listResult.offsetWidth/2 + "px";
